8710f1e772814df0007a72285b714b239897b24e
Fede112 <fedebarone@gmail.com>
2021-11-17 17:46:39 +0100

    Added download links to downloads.php

--- templates/downloads.php
+++ templates/downloads.php
@@ -25,32 +25,46 @@
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-download" viewBox="0 0 16 16">
   				<path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z"></path>
   				<path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z"></path>
-			  </svg>
+			  	</svg>
+			  	
 			  </td>
 		    </tr>
 		    <tr>
 		      <td>DPCfam cdhit-0.60 seeds</td>
 		      <td>DPCfam classification of the UniRef50 database (v. 201707) filtered using 60% as the similarity threshold in cdhit.</td>
-		      <td><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-download" viewBox="0 0 16 16">
+
+		      <td>
+		      	<a href="<?= path2url($config->paths->bulkfiles."cdhit/dpcfam_cdhit_seeds.zip"); ?>" download="dpcfam_cdhit_seeds.zip">
+		      	<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-download" viewBox="0 0 16 16">
   				<path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z"></path>
   				<path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z"></path>
-			  </svg></td>
+			  	</svg>
+			  	</a>
+			  </td>
 		    </tr>
 		    <tr>
 		      <td>DPCfam MSA</td>
 		      <td>Multiple sequence alignment of each Metacluster using DPCfam cdhit-060 seeds as input.</td>
-		      <td><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-download" viewBox="0 0 16 16">
+		      <td>
+		      	<a href="<?= path2url($config->paths->bulkfiles."msa/dpcfam_msas.zip"); ?>" download="dpcfam_msas.zip">
+		      	<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-download" viewBox="0 0 16 16">
   				<path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z"></path>
   				<path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z"></path>
-			  </svg></td>
+			  	</svg>
+			  	</a>
+			  </td>
 		    </tr>
 		    <tr>
 		      <td>DPCfam HMM profiles</td>
-		      <td>HMM-profiles for all MCs</td>
-		      <td><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-download" viewBox="0 0 16 16">
+		      <td>HMM-profiles for all MCs.</td>
+		      <td>
+		      	<a href="<?= path2url($config->paths->bulkfiles."hmm/dpcfam_hmms.zip"); ?>" download="dpcfam_hmms.zip">
+		      	<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-download" viewBox="0 0 16 16">
   				<path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z"></path>
   				<path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z"></path>
-			  </svg></td>
+			  	</svg>
+			  	</a>
+			  </td>
 		    </tr>
 		    </tbody>
 
