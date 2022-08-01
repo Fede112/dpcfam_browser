d9ad9aeb2f2091339a492bd7e7302b98214fa204
gonzam88 <gonzamoiguer@gmail.com>
2022-08-01 19:44:09 -0300

    animacion fondo

--- templates/home.php
+++ templates/home.php
@@ -28,6 +28,23 @@ if(session_id() == '' || !isset($_SESSION) || session_status() === PHP_SESSION_N
 
 <!-- Begin page content -->
 <main class="flex-shrink-0" id="search-box-container">
+    <div id="homeBg">
+        <?php
+        for($row=0; $row<11; $row++){
+            ?>
+            <div class="fila">
+                <div class="coso"></div>
+                <div class="coso"></div>
+                <div class="coso"></div>
+                <div class="coso"></div>
+                <div class="coso"></div>
+            </div>
+
+            <?php
+        }
+         ?>
+    </div>
+
 	<div class="container">
 		<h1 class="mt-5" style="color:white">DPCfam search</h1>
 		<div id="search_div">
@@ -48,7 +65,7 @@ if(session_id() == '' || !isset($_SESSION) || session_status() === PHP_SESSION_N
 					<!-- <img  class="loader_anim" style="float:right;" id='loading' width="100px" src="http://rpg.drivethrustuff.com/shared_images/ajax-loader.gif"/>  -->
 
 					<!-- <button type="submit" class="btn btn-primary" style="background-color:#134F5C;"> -->
-					<button type="submit" class="btn btn-primary" style="background-color:#098180;">
+					<button type="submit" class="btn btn-primary" id="search-button">
 						<svg class="svg-inline--fa fa-search fa-w-16" aria-hidden="true" data-prefix="fas" data-icon="search" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
 							<path fill="currentColor" d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"></path>
 						</svg>
@@ -66,11 +83,14 @@ if(session_id() == '' || !isset($_SESSION) || session_status() === PHP_SESSION_N
 
 		</div> <!-- search_div -->
 
-		<div id="intro">
-			.
-		</div id="intro">
+    </div>
+
+
+
+</main>
 
 		<!-- SEARCH RESULTS -->
+        <div class="container"
 		<?php
 		// All queries, independently of the category, will output a set of MCs pages stored in $mc_matches
 		if(isset($_GET["search_input"]) )
@@ -239,23 +259,51 @@ if(session_id() == '' || !isset($_SESSION) || session_status() === PHP_SESSION_N
 
 
 
-
+        </div>
 		<?php
 
 		}else
 
 		{
 			// NO $search_input!!
+            ?>
+
+            <div class="container" id="home-about">
+                <!-- ROW #1 -->
+                <div class="row">
+                    <div class="col-6">
+                    Characterizing the function of proteins is an extremely important step in understanding biological processes. This characterization can be performed, in principle, by dedicated experiments, both in a wet lab or through computational simulations which are overall expensive. The number of proteins that have been characterized with such methods is still relatively small. Instead, in the last decades the number of protein sequences known has grown exponentially especially thanks to high throughput genomic sequencing experiments.
+                    </div>
+                    <div class="col">
+
+                    </div>
+              </div>
+              <!-- ROW #2 -->
+              <div class="row">
+                  <div class="col">
+
+                  </div>
+                  <div class="col-6">
+                  Proteins that are known only at a sequence level outnumber those with an experimental characterization by orders of magnitude and we expect this gap to increase over the next years. In this context we introduce DPCfam [1], a new unsupervised procedure that uses sequence alignments and Density Peak Clustering [2] to automatically classify homologous protein regions . DPCfam shows potential both for assisting manual annotation efforts (domain discovery, detection of classification inconsistencies, improvement of family coverage and boosting of clan membership) and as a stand-alone tool for unsupervised classification of sparsely annotated protein datasets such as those from environmental metagenomics studies (domain discovery, analysis of domain diversity).
+                  </div>
+            </div>
+            <!-- ROW #3 -->
+            <div class="row">
+                <div class="col-6">
+                In its current version we ran the DPCfam algorithm to generate clusters on the UniRef50 database, which contains representatives of all known protein sequences (sharing less than 50% similarity). The classification is evolutionary accurate and it covers a significant fraction of known homologs annotated in Pfam [3]. Moreover, DPCfam suggests the classification of previously unknown regions, tagged as UNK, some of which have been already added to the latest version of the Pfam database.
+                </div>
+                <div class="col">
+
+                </div>
+          </div>
+            </div>
+            <?php
 		}
 		?>
 
 
 
-	</div>
 
-
-
-</main>
 
 
 
@@ -295,4 +343,36 @@ $('#target').change(function(){
     }
   // trigger change on page load to set initial placeholder
 }).change()
+
+let filas;
+let maxChange = 12;
+
+function ReordenarFilas(){
+    for(let i=0; i< filas.length; i++){
+        let tamanos = [20,20,20,20,20]
+        for(let j=0; j < filas[i].children.length -1 ; j++){
+            let cambio = (Math.random() - 0.5) * maxChange
+            tamanos[j] += cambio;
+            tamanos[j+1] -= cambio;
+        }
+        for(let j=0; j < filas[i].children.length ; j++){
+            filas[i].children[j].style.width = "calc("+tamanos[j]+"% - 5px)"
+            // console.log(`calc(${tamanos[j]}% - 5px);`)
+        }
+        // console.log(tamanos)
+
+    }
+    // columnas.forEach((col, i) => {
+    //     col.style.gridTemplateColumns = ".3fr 1.7fr 1fr 1fr 2fr"
+    // });
+}
+
+window.addEventListener("load", function(){
+    filas = document.getElementsByClassName("fila")
+    ReordenarFilas();
+    setInterval(ReordenarFilas, 3500)
+})
+
+
+
 </script>
